5be4b925519809a9bdc9d5c169468ee1d6fb9c28
JoSantana0005 <joseasantana05@gmail.com>
2025-04-09 16:51:52 -0400

    Arreglando cosas

--- SMO/Components/Content/ContentGallery.jsx
+++ SMO/Components/Content/ContentGallery.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableHighlight, FlatList } from "react-native";
 import { StylesGallery } from "../../Css/Gallery";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
@@ -23,9 +23,26 @@ export function ContentGallery(){
     }, []);
     return(
         <View style={StylesGallery.ContainerGallery}>
-            <View style={StylesGallery.ContainerGalleryHeader}>
-                <Text style={StylesGallery.TextGallery}>Galeria</Text>
-                <Image style={StylesGallery.ImageHeader}/>
+            <View style={StylesGallery.ContainerMain}>
+                <View style={StylesGallery.ContainerGalleryHeader}>
+                    <Text style={StylesGallery.TextGallery}>Galeria</Text>
+                    <Image style={StylesGallery.ImageHeader} source={require('../../assets/Gallery/Gallery.png')}/>
+                </View>
+                <View style={StylesGallery.ContainerImages}>
+                    <FlatList
+                        keyExtractor={(item, index) => index.toString()}
+                        data={listImage}
+                        renderItem = {({item}) => (
+                                <View style={StylesGallery.ContainerImage}>
+                                    <Image style={StylesGallery.ImageContainer} source={{uri: item}}/>
+                                    <TouchableHighlight style={StylesGallery.ButtonImage} onPress={() => {}}>
+                                        <Text style={StylesGallery.textImage}>Ver Imagen</Text>
+                                    </TouchableHighlight>
+                                </View>
+                        )}
+                        showsHorizontalScrollIndicator={false}
+                    />
+                </View>
             </View>
         </View>
     )
--- SMO/Css/Gallery.js
+++ SMO/Css/Gallery.js
@@ -6,17 +6,72 @@ export const StylesGallery = StyleSheet.create({
         padding: 5,
         alignItems: 'center'
     },
+    ContainerMain: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+    },
     ContainerGalleryHeader:{
         borderColor: '#fff',
         borderWidth: 2,
         width: '100%',
+        height: '5%',
         paddingHorizontal: 5,
-        flexDirection: 'row'
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
     TextGallery:{
         fontSize: 18,
         fontWeight: '500',
         color: '#eee',
         letterSpacing: 2
+    },
+    ImageHeader: {
+        width: 40,
+        height: 40,
+        resizeMode: 'cover'
+    },
+    ContainerImages: {
+        borderColor: '#fff',
+        borderWidth: 1,
+        width: '100%',
+        height: '95%',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center'
+    },
+    ContainerImage: {
+        width: '100%',
+        height: 120,
+        padding: 2,
+        alignItems: 'center',
+        gap: 3
+    },
+    ImageContainer: {
+        borderColor: '#ddaa',
+        borderWidth: 1,
+        borderRadius: 10,
+        width: '100%',
+        height: '85%',
+        resizeMode: 'cover'
+    },
+    ButtonImage: {
+        borderColor: '#ddaa',
+        borderWidth: 1,
+        width: '65%',
+        height: '15%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 15,
+        backgroundColor: '#222',
+
+    },
+    textImage: {
+        fontSize: 13,
+        fontWeight: '500',
+        color: '#eee',
+
     }
+
 })
